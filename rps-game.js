17ad8a519502e301d525d9d7d99818e25d6caafa
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-16 15:34:31 -0300

    Add a function to display one out of three words

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,8 @@
 console.log("Hello");
+function getComputerChoice() {
+    let rps = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * 3);
+    console.log(random, rps[random]);
+}
+
+getComputerChoice();
