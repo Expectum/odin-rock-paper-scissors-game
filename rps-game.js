5a2dca5745e53ff6751f430d47e6c902e6b51e4e
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-16 22:17:12 -0300

    Add game function to play a game of 5 rounds

--- rps-game.js
+++ rps-game.js
@@ -1,22 +1,31 @@
-console.log("Hello");
-function getComputerChoice() {
-    let rps = ["Rock", "Paper", "Scissors"];
-    const random = Math.floor(Math.random() * 3);
-    return rps[random];
-}
-
-let playerInput = window.prompt("Enter rock, paper or scissors.","");
-let char = playerInput.charAt(0).toUpperCase();
-let playerSelection = char + playerInput.toLowerCase().slice(1);
-let computerSelection = getComputerChoice();
-function playRound(playerSelection, computerSelection) {
-if (playerSelection == computerSelection) {
-    return ("You Draw! " + computerSelection + " ties with " + playerSelection + ".");
-} else if (((playerSelection == "Paper") && (computerSelection == "Rock")) || 
-((playerSelection == "Rock") && (computerSelection == "Scissors")) || 
-((playerSelection == "Scissors") && (computerSelection == "Paper"))){
-    return ("You Win! " + playerSelection + " beats " + computerSelection + ".");
+function playRound(playerSelection, computerSelection, playerScore) {
+    if (playerSelection == computerSelection) {
+        console.log("You Draw! " + computerSelection + " ties with " + playerSelection + ".");
+    } else if (((playerSelection == "Paper") && (computerSelection == "Rock")) || 
+    ((playerSelection == "Rock") && (computerSelection == "Scissors")) || 
+    ((playerSelection == "Scissors") && (computerSelection == "Paper"))){
+        console.log("You Win! " + playerSelection + " beats " + computerSelection + ".");
+        return playerScore += 1;
+    } else {
+        console.log("You Lose! " + computerSelection + " beats " + playerSelection + ".");
+    }};
+function game() {
+let playerScore = 0;
+for (let i = 0; i < 5; i++) {
+    function getComputerChoice() {
+        let rps = ["Rock", "Paper", "Scissors"];
+        const random = Math.floor(Math.random() * 3);
+        return rps[random];
+    }
+    let computerSelection = getComputerChoice();
+    let playerInput = window.prompt("Enter rock, paper or scissors.","");
+    let char = playerInput.charAt(0).toUpperCase();
+    let playerSelection = char + playerInput.toLowerCase().slice(1);
+    playRound(playerSelection, computerSelection, playerScore);
+}     if (playerScore > 2) {
+    console.log("You Win the Game!");
 } else {
-    return ("You Lose! " + computerSelection + " beats " + playerSelection + ".");
-}};
-console.log(playRound(playerSelection, computerSelection));
+    console.log("You Lose the Game!");
+}}
+game();
+
