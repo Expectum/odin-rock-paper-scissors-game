abdba93452fe1e798c443dc4da83a1b0ba941254
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-21 17:41:40 -0300

    Add an event listener to the buttons to do a round

--- rps-game.js
+++ rps-game.js
@@ -12,24 +12,40 @@ function playRound(playerSelection, computerSelection, playerScore) {
         return playerScore;
     }};
 function game() {
-    let playerScore = 0;
-    function getComputerChoice() {
-        let rps = ["Rock", "Paper", "Scissors"];
-        const random = Math.floor(Math.random() * 3);
-        return rps[random];
-    }
+
     let computerSelection = getComputerChoice();
     let playerInput = window.prompt("Enter rock, paper or scissors.","");
     let char = playerInput.charAt(0).toUpperCase();
-    let playerSelection = char + playerInput.toLowerCase().slice(1);
+    let playerSelection = button1 || button2 || button3;
     playerScore = playRound(playerSelection, computerSelection, playerScore);
 }     
+function getComputerChoice() {
+    let rps = ["Rock", "Paper", "Scissors"];
+    const random = Math.floor(Math.random() * 3);
+    return rps[random];
+}
+let playerScore = 0;
 const container = document.querySelector('#container');
 const button1 = document.createElement('button');
 const button2 = document.createElement('button');
 const button3 = document.createElement('button');
+let playerSelection;
+button1.addEventListener('click', () => {
+    playerSelection = "Rock";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection, playerScore);
+});
+button2.addEventListener('click', () => {
+    playerSelection = "Paper";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection, playerScore);
+});
+button3.addEventListener('click', () => {
+    playerSelection = "Scissors";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection, playerScore);
+});
 container.appendChild(button1);    
 container.appendChild(button2);     
 container.appendChild(button3);        
-game();
 
